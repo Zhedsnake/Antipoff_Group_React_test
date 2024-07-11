b41a017a00e008e828cad1b910be0acebd27b27a
Zhedsnake <besedinzena5@gmail.com>
2024-07-11 14:58:18 +0300

    1

---
+++ client/src/components/UserList.tsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react';
+import { useAppSelector, useAppDispatch } from '../hooks';
+import { fetchUsers } from '../features/users/usersSlice';
+
+const UserList: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const { users, loading, error } = useAppSelector((state) => state.users);
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>{error}</div>;
+
+  return (
+    <div>
+      {users.map((user) => (
+        <div key={user.id}>{user.name}</div>
+      ))}
+    </div>
+  );
+};
+
+export default UserList;
