8776970264bd68edb61a57587fa0644a47b44753
Zhedsnake <besedinzena5@gmail.com>
2024-07-13 16:35:06 +0300

    Добавление панинации на главно

--- client/src/Sclices/users/usersSlice.ts
+++ client/src/Sclices/users/usersSlice.ts
@@ -11,18 +11,23 @@ interface User {
 interface UsersState {
   users: User[];
   status: 'idle' | 'succeeded';
+  currentPage: number,
 }
 
 const initialState: UsersState = {
   users: [],
   status: 'idle',
+  currentPage: 1,
 };
 
 // Асинхронное действие для получения пользователей
-export const getUsers = createAsyncThunk('users/getUsers', async () => {
-  const users = await fetchUsers();
-  return users;
-});
+export const getUsers = createAsyncThunk(
+  'users/getUsers',
+  async (page: number) => {
+    const users = await fetchUsers(page);
+    return users;
+  }
+);
 
 const usersSlice = createSlice({
   name: 'users',
@@ -33,7 +38,7 @@ const usersSlice = createSlice({
       .addCase(getUsers.fulfilled, (state, action) => {
         state.status = 'succeeded';
         state.users = action.payload;
-      })
+      });
   },
 });
 
--- client/src/components/App/UserList.tsx
+++ client/src/components/App/UserList.tsx
@@ -1,18 +1,28 @@
 import React, { useEffect } from 'react';
+import { Link } from "react-router-dom";
+
 import { useAppDispatch, useAppSelector } from '../../hooks/hooks';
+
 import { getUsers } from '../../sclices/users/usersSlice';
+
+import { API_URL } from '../../api/config';
 
 
 const UserList: React.FC = () => {
   const dispatch = useAppDispatch();
   const users = useAppSelector((state) => state.users.users);
   const status = useAppSelector((state) => state.users.status);
+  const currentPage = useAppSelector((state) => state.users.currentPage);
 
   useEffect(() => {
     if (status === 'idle') {
-      dispatch(getUsers());
+      dispatch(getUsers(currentPage));
     }
-  }, [status, dispatch]);
+  }, [status, dispatch, currentPage]);
+
+  const handlePageChange = (page: number) => {
+    dispatch(getUsers(page));
+  };
 
   let content;
 
@@ -21,23 +31,33 @@ const UserList: React.FC = () => {
       <div className="app__users-container">
         <ul className="app__users-list">
           {users.map((user) => (
-            <li key={user.id} className="app__user-list-item user-list-item">
-              <div className="user-list-item__container">
-                <div className="user-list-item__image-container">
-                  <img className="user-list-item__image" src={user.avatar} alt={`${user.first_name} ${user.last_name}`} />
-                </div>
-                <h3 className="user-list-item__name">
-                  {user.first_name} {user.last_name}
-                </h3>
-              </div>
-            </li>
+            <Link to={`${API_URL}/api/users/${user.id}`}>
+              <li key={user.id} className="app__user-list-item user-list-item">
+                  <div className="user-list-item__container">
+                    <div className="user-list-item__image-container">
+                      <img className="user-list-item__image" src={user.avatar} alt={`${user.first_name} ${user.last_name}`} />
+                    </div>
+                    <h3 className="user-list-item__name">
+                      {user.first_name} {user.last_name}
+                    </h3>
+                  </div>
+              </li>
+            </Link>
           ))}
         </ul>
       </div>
     );
   }
 
-  return <section className="app__users">{content}</section>;
+  return (
+    <section className="app__users">
+      {content}
+      <div className="app__pagination pagination">
+          <button className="pagination__button" onClick={() => handlePageChange(1)}>1</button>
+          <button className="pagination__button" onClick={() => handlePageChange(2)}>2</button>
+      </div>
+    </section>
+  );
 };
 
 export default UserList;
